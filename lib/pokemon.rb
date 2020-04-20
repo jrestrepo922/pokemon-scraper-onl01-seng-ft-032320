6b9610959b25099a586ec2e07111ac7f6061834e
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-20 18:46:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,6 +18,6 @@ class Pokemon
   def self.find(pokemon_id, database)
     array = database.execute("SELECT * FROM pokemon WHERE id = ? ", pokemon_id)[0]
     binding.pry
-    #pokemon = Pokemon.new(id:array[0], name:array[1], type:electric[2])
+    pokemon = Pokemon.new(id:array[0], name:array[1], type:electric[2])
   end
 end
